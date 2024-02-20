3e039e5f1200f972e7a90fbc17ae14415b93e2d0
Saif Amjad <saifamjad03@gmail.com>
2024-02-20 20:22:19 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { CookiesProvider } from 'react-cookie';
 import { GoogleOAuthProvider } from '@react-oauth/google';
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ <CookiesProvider>
   <GoogleOAuthProvider clientId="700535868110-vmptg7hjd8uh6t7onp693nt442npqm4f.apps.googleusercontent.com" >
   <React.StrictMode>
    
@@ -14,6 +16,7 @@ root.render(
    
   </React.StrictMode>
   </GoogleOAuthProvider>
+</CookiesProvider>
 );
 
 
