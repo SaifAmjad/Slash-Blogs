ce291c327690509c1d27a957c5774f32cdcc017b
Saif Amjad <saifamjad03@gmail.com>
2024-02-22 21:08:59 +0500

    Update BlogState.js

--- src/context/BlogState.js
+++ src/context/BlogState.js
@@ -84,7 +84,6 @@ const BlogState = (props) => {
                 }
             });
             const data=await res.json();
-            console.log(data)
             return data;
         } catch (error) {
             console.log(error);
